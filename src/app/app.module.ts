c9f1a144e28bb63218292723303068a01dca9a1d
alce65 <alce65@hotmail.es>
2019-11-26 13:24:13 +0100

    saludo: vistas en Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
+import { NgbModule} from '@ng-bootstrap/ng-bootstrap'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,9 @@ import { CoreModule } from './core/core.module';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
+    NgbModule,
     FontAwesomeModule,
     CoreModule
   ],
---
+++ src/app/home/saludo/saludo.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Event } from '@angular/router';
+
+@Component({
+  selector: 'aub-saludo',
+  templateUrl: './saludo.component.html',
+  styleUrls: ['./saludo.component.css']
+})
+export class SaludoComponent implements OnInit {
+  nombre: string;
+  mensaje: string;
+  constructor() { }
+
+  ngOnInit() {
+    this.mensaje = 'Aqui va tu nombre'
+  }
+
+  onClickBorrar(ev: Event) {
+    console.log(ev)
+    this.nombre = ''
+  }
+
+}
