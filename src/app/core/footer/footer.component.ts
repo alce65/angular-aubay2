9260c1e32ab5731bb05372977f0b24de8e30f4f2
alce65 <alce65@hotmail.es>
2019-11-26 10:59:04 +0100

    Modulo Core inicial

---
+++ src/app/core/footer/footer.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'aub-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent implements OnInit {
+  autor: string;
+  fecha: Date;
+  constructor() { }
+
+  ngOnInit() {
+    this.autor = 'Alejandro Cerezo'
+    this.fecha = new Date();
+  }
+
+}
