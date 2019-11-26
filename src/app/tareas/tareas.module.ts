cb67e426157fb09951554cc37ded77e198d8d069
alce65 <alce65@hotmail.es>
2019-11-26 13:38:34 +0100

    Tareas y subrouting

--- src/app/tareas/tareas.module.ts
+++ src/app/tareas/tareas.module.ts
@@ -3,10 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { TareasRoutingModule } from './tareas-routing.module';
 import { MainComponent } from './main/main.component';
+import { MenuComponent } from './menu/menu.component';
+import { SimpleComponent } from './simple/simple.component';
+import { PlusComponent } from './plus/plus.component';
 
 
 @NgModule({
-  declarations: [MainComponent],
+  declarations: [MainComponent, MenuComponent, SimpleComponent, PlusComponent],
   imports: [
     CommonModule,
     TareasRoutingModule
