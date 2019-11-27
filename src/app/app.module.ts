9fb02bfe9763250c96f79e381a4baccd5fcb37a0
alce65 <alce65@hotmail.es>
2019-11-27 17:50:21 +0100

    Pipes nativos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NgbModule} from '@ng-bootstrap/ng-bootstrap'
 import { AppRoutingModule } from './app-routing.module';
@@ -7,6 +7,10 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { FormsModule } from '@angular/forms';
 import { SharedModule } from './shared/shared.module';
+import es from '@angular/common/locales/es'
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(es, 'es')
 
 @NgModule({
   declarations: [
@@ -21,7 +25,7 @@ import { SharedModule } from './shared/shared.module';
     CoreModule,
     SharedModule
   ],
-  providers: [],
+  providers: [ {provide: LOCALE_ID, useValue: 'es'}],
   exports: [],
   bootstrap: [AppComponent]
 })
