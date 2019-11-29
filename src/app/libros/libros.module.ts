f0c06a5fe04a70dea6ddcb2f2786fba6099eb754
alce65 <alce65@hotmail.es>
2019-11-29 13:40:53 +0100

    API Libros

--- src/app/libros/libros.module.ts
+++ src/app/libros/libros.module.ts
@@ -7,14 +7,19 @@ import { MockComponent } from './mock/mock.component';
 import { SharedModule } from '../shared/shared.module';
 import { GoogleComponent } from './google/google.component';
 import { LibrosMockService } from '../services/libros-mock.service';
+import { ByServiceComponent } from './by-service/by-service.component';
+import { LibrosGoogleService } from '../services/libros-google.service';
 
 @NgModule({
-  declarations: [LibrosComponent, MockComponent, GoogleComponent],
+  declarations: [LibrosComponent, MockComponent, GoogleComponent, ByServiceComponent],
   imports: [
     CommonModule,
     SharedModule,
     LibrosRoutingModule
   ],
-  providers: [LibrosMockService]
+  providers: [
+    LibrosMockService,
+    LibrosGoogleService
+  ]
 })
 export class LibrosModule { }
